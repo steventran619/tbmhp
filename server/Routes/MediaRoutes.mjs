ce68b150d5ceba251d9bdfccb9ed50e0117c4bfc
Steven Tran <dpoopd@gmail.com>
2023-11-10 16:25:51 -0800

    Added try catch clause

--- server/Routes/MediaRoutes.mjs
+++ server/Routes/MediaRoutes.mjs
@@ -1,25 +1,27 @@
 import express from 'express';
 import { Image } from '../Models/ImagesModel.mjs';
-import db from '../Middlewares/DbConnection.mjs';
+import { connectMongo } from '../Middlewares/DbConnection.mjs';
 import UpdateMedia from '../Controllers/InstagramController.mjs';
 
 // Router for "instagram-media" MongoDB db
 const router = express.Router();
 
-router.get('/', async(req, res) => {
-    let collection = await db.collection("images");
-    let results = await collection.find({Image}).toArray();
-    res.send(results).status(200);
-});
-
-router.get('/update', async(req, res) => {
+router.get('/', async (req, res) => {
+    console.log("Images requested");
     try {
-        let mediaResults = UpdateMedia();
-        res.send(mediaResults).status(200);
+        let connection = await connectMongo();
+        let collection = connection.collection('images');
+        let results = await collection.find({ Image }).toArray();
+        res.send(results).status(200);
     } catch (error) {
-        console.log(error);
+        console.log(error)
         res.status(400).json({ error: 'Request to retrieve document failed' });
-    }    
+    }
 });
 
+router.get("/update", async (req, res) => {
+    console.log("Update requested");
+    let mediaResults = UpdateMedia();
+    res.send(mediaResults).status(200);
+});
 export default router;
