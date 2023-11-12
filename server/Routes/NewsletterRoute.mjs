32b04e9be91724d979e2ef4aed6e59af9dc3dd9f
DillonOdle <dillonodle@gmail.com>
2023-11-11 19:48:28 -0800

    Redirect the newsletter subscriber to the success or failure page based on whether they were able to verify their email.

--- server/Routes/NewsletterRoute.mjs
+++ server/Routes/NewsletterRoute.mjs
@@ -12,6 +12,17 @@ const NewsletterSubscriber = createNewsletterSubscriberModel(newsletterConn);
 const router = express.Router();
 
 router.post('/signup', (req, res, next) => Signup(NewsletterSubscriber, req, res, next));
-router.get('/verify/:token', (req, res, next) => Verify(NewsletterSubscriber, req, res, next));
+
+router.get('/verify/:token', async (req, res) => {
+  try {
+    const result = await Verify(NewsletterSubscriber, req.params.token);
+    if (result.success) {
+      res.redirect(`${process.env.APP_BASE_URL}/newsletter-success`);
+    }
+  } catch (error) {
+    const message = error.message;
+    res.redirect(`${process.env.APP_BASE_URL}/newsletter-failure?message=${encodeURIComponent(message)}`);
+  }
+});
 
 export default router;
