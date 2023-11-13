1af36e3214db1e49af43423d1bd1c6538f7673d8
DillonOdle <dillonodle@gmail.com>
2023-11-12 19:41:30 -0800

    Updated the Signup and Verify functions to be a bit cleaner and work with the changes to the sendEmail and createSecretToken functions

--- server/Controllers/NewsletterController.mjs
+++ server/Controllers/NewsletterController.mjs
@@ -17,27 +17,29 @@ export async function Signup(NewsletterSubscriber, req, res, next) {
     const existingSubscriber = await NewsletterSubscriber.findOne({ email });
     let subscriber;
     // If the user has an active account (already verified, return)
-    if (existingSubscriber) {
-      if (existingSubscriber.active) {
-        return res.json({ message: "This email is already subscribed" });
-      } else {
-        subscriber = existingSubscriber;
-      }
-    } else {
-      subscriber = await NewsletterSubscriber.create({ email, firstname, lastname });
+    if (existingSubscriber && existingSubscriber.active) {
+      return res.json({ message: "This email is already subscribed" });
     }
 
-    const token = createSecretToken(subscriber._id);
+    subscriber = existingSubscriber || await NewsletterSubscriber.create({ email, firstname, lastname });
 
-    sendVerification(firstname, email, token)
-    .then(() => {
-      res.status(201).json({ message: "Verification Email Sent", success: true, subscriber });
-    })
-    .catch(err => {
-      console.error('An error occurred: ', err);
-    })
+
+    // Create jwt token to send with email
+    const jwtExpireTime = 3 * 24 * 60 * 60;
+    const token = createSecretToken({id: subscriber._id, email: email}, jwtExpireTime);
     
-
+    // Send email
+    const verificationUrl = `${process.env.SERVER_BASE_URL}/newsletter/verify/${token}`;
+    const verificationSubject = 'TBMHP Newsletter Verification'
+    const verificationMessage = `
+      <h1>Hi ${firstname}</h1>
+      <p>Thank you for subscribing to our newsletter. 
+        Please follow the <a href="${verificationUrl}">Link</a> to verify your email.</p>
+      <p>TBMHP Team</p>
+      `;
+    
+    await sendEmail(email, verificationSubject, verificationMessage);
+    res.status(201).json({ message: "Verification Email Sent", success: true, subscriber });
     next();
   } catch (error) {
     console.error(error);
@@ -52,11 +54,37 @@ export async function Verify(NewsletterSubscriber, token) {
 
   try {
     const decoded = jwt.verify(token, process.env.TOKEN_KEY);
+    const email = decoded.email;
     const subscriber = await NewsletterSubscriber.findByIdAndUpdate(decoded.id, { active: true }, { new: true });
 
     if (!subscriber) {
       throw new Error('Subscriber not found');
     }
+
+        // Create jwt token (expires in 1 year) to send for unsubscription email
+    const jwtExpireTime = 365 * 24 * 60 * 60;
+    const unsubscribeToken = createSecretToken({id: subscriber._id, email: subscriber.email}, jwtExpireTime);
+    
+    // Send confirmation email
+    const unsubscribeUrl = `${process.env.SERVER_BASE_URL}/newsletter/unsubscribe/${unsubscribeToken}`;
+    const confirmationSubject = 'TBMHP Newsletter Confirmation'
+    const confirmationMessage = `
+      <h1>Welcome!</h1>
+      <p>Thank you for subscribing to the TBMHP newsletter. Your subscription is now confirmed.</p> 
+        
+      <p>If you would like to unsubscribe, please follow this <a href="${unsubscribeUrl}">Link</a>.</p>
+      <p>TBMHP Team</p>
+      `;
+    
+    await sendEmail(email, confirmationSubject, confirmationMessage);
+
+    return { success: true };
+  } catch (error) {
+    // Log the error and rethrow it
+    console.error(error);
+    throw error;
+  }
+}
 
     return { success: true };
   } catch (error) {
