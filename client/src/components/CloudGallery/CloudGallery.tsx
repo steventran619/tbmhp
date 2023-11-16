691a6ec8da1c6304f5deb18206ae8c4b36633bec
Steven Tran <dpoopd@gmail.com>
2023-11-16 15:43:51 -0800

    TitleCased custom gallery folder names

--- client/src/components/CloudGallery/CloudGallery.tsx
+++ client/src/components/CloudGallery/CloudGallery.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { CloudinaryAlbum } from '../../types';
 import { SimpleGrid, Image, Loader, Divider } from '@mantine/core';
+import { titleCase } from '../../utilities/titleCase';
 
 
 export const CloudGallery = (): React.ReactElement => {
@@ -33,7 +34,7 @@ export const CloudGallery = (): React.ReactElement => {
             {result.data?.map((album: CloudinaryAlbum, index: number) => (
                 <div key={index}>
                     <Divider my="xl" size="md"/>
-                    <h2>{album.album}</h2>
+                    <h2>{titleCase(album?.album)}</h2>
                     <SimpleGrid
                         cols={{ base: 1, sm: 2, lg: 3 }}
                         spacing={{ base: 5, sm: 'xl' }}
