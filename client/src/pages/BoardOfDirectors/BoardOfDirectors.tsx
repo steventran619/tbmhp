dff3f6e022bad1937551937eb71f338e9bd5ad76
Steven Tran <dpoopd@gmail.com>
2023-11-25 00:16:32 -0800

    Added BoardOfDirectors and Contact page

---
+++ client/src/pages/BoardOfDirectors/BoardOfDirectors.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const BoardOfDirectors = (): React.ReactElement => {
+    return (
+        <div>
+            <h1>Board of Directors</h1>
+            <p>Cards of each board member will go here.</p>
+        </div>
+    );
+}
