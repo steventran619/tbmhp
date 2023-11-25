4458f891b263905adfed08885857a66eb672031c
Steven Tran <dpoopd@gmail.com>
2023-11-25 14:28:28 -0800

    Fixed elements from being hidden on homepage

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { HeroImageLeft } from '../../components/HeroImageLeft/HeroImageLeft';
+import classes from './Home.module.css';
 
 export const Home = (): React.ReactElement => {
     return (
-        <>
+        <div className={classes.wrapper}>
             <h1>Thomas Batterman Mental Health Project</h1>
             <HeroImageLeft />
-        </>
+        </div>
     );
 }
