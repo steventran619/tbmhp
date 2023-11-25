b0a7a073f843b56843cb3687dffcd0b159f14ec7
Steven Tran <dpoopd@gmail.com>
2023-11-24 16:32:54 -0800

    Added unescaped single quote

--- client/src/pages/UnsubscribeSuccess/UnsubscribeSuccess.tsx
+++ client/src/pages/UnsubscribeSuccess/UnsubscribeSuccess.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const UnsubscribeSuccess = (): React.ReactElement => {
   return (
     <div className="newsletter-success">
-      <h1>We're sorry to see you go!</h1>
+      <h1>We&apos;re sorry to see you go!</h1>
       <p>You have successfully unsubscribed from our newsletter.</p>
     </div>
   );
