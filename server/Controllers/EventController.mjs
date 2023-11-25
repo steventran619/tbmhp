340fd350a6ea3f2f5fbe548acf506c15fabc7d7a
Steven Tran <dpoopd@gmail.com>
2023-11-24 22:45:21 -0800

    Fixed past events routing

--- server/Controllers/EventController.mjs
+++ server/Controllers/EventController.mjs
@@ -5,7 +5,23 @@ const eventBriteToken = process.env.EVENTBRITE_API_PRIVATE_TOKEN;
 
 export async function Events(req, res) {
     try {
-        const response = await axios.get(`https://www.eventbriteapi.com/v3/organizations/${eventBriteOrganizationId}/events/`, {
+        const response = await axios.get(`https://www.eventbriteapi.com/v3/organizations/${eventBriteOrganizationId}/events/?time_filter=current_future`, {
+            headers: {
+                Authorization: `Bearer ${eventBriteToken}`
+            }
+        });
+        console.log(response.data);
+        res.status(200).json(response.data);
+    } catch (error) {
+        // Log the error and rethrow it
+        console.error(error);
+        res.status(400).json({ error: 'Request to retrieve events failed' })
+    }
+}
+
+export async function PastEvents(req, res) {
+    try {
+        const response = await axios.get(`https://www.eventbriteapi.com/v3/organizations/${eventBriteOrganizationId}/events/?time_filter=past`, {
             headers: {
                 Authorization: `Bearer ${eventBriteToken}`
             }
--- server/Routes/EventRoutes.mjs
+++ server/Routes/EventRoutes.mjs
@@ -1,8 +1,9 @@
 import express from 'express';
-import { Events } from '../Controllers/EventController.mjs';
+import { Events, PastEvents } from '../Controllers/EventController.mjs';
 
 const router = express.Router();
 
 router.get('/', (req, res) => Events(req, res));
+router.get('/past', (req, res) => PastEvents(req, res));
 
 export default router;
