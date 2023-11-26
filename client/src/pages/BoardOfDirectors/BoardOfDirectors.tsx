b56578b4365874ca178fa91f7da6270cfaccc0ec
Steven Tran <dpoopd@gmail.com>
2023-11-26 11:37:14 -0800

    Added padding to prevent text overflow

--- client/src/pages/BoardOfDirectors/BoardOfDirectors.tsx
+++ client/src/pages/BoardOfDirectors/BoardOfDirectors.tsx
@@ -3,6 +3,7 @@ import { ProfileCard } from '../../components/ProfileCard/ProfileCard';
 import { Title, Text, Space } from '@mantine/core';
 import { Profile } from '../../types';
 import profilesData from './board.json';
+import classes from './BoardOfDirectors.module.css';
 
 export const BoardOfDirectors = (): React.ReactElement => {
     const [profiles, setProfiles] = React.useState<Profile[]>([]);
@@ -13,12 +14,14 @@ export const BoardOfDirectors = (): React.ReactElement => {
 
 
     return (
-        <div>
+        <div className={classes.wrapper}>
             <Title order={1} ta="center">Board of Directors</Title>
             <Space h="lg" />
             <Text size="lg" fw={600} ta="center">Meet our <Text span variant="gradient"
-                gradient={{ from: 'indigo', to: 'green', deg: 75 }} inherit>amazing</Text> team</Text>
-            <Text size="lg" fw={400} ta="center">Our team is a mix of Tom&#39;s family and friends that are committed to the success of TBMHP and the
+                gradient={{ from: 'indigo', to: 'green', deg: 75 }} inherit>
+                amazing
+            </Text> team</Text>
+            <Text size="lg" fw={400} ta="center" className={classes.description}>Our team is a mix of Tom&#39;s family and friends that are committed to the success of TBMHP and the
                 mission that we strive to obtain.
             </Text>
             {profiles.map((person) => (
