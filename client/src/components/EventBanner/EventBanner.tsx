976c2a2c7df7c4c902ac0385507b59f8aab02abe
Steven Tran <dpoopd@gmail.com>
2023-11-25 21:19:45 -0800

    Added EventBanner if an upcoming event exists

--- client/src/components/EventBanner/EventBanner.tsx
+++ client/src/components/EventBanner/EventBanner.tsx
@@ -1,8 +1,7 @@
-import { Card, Overlay, Button, Text, Grid } from '@mantine/core';
+import { Container, Button, Text, Group, Badge, Box, BackgroundImage, Space } from '@mantine/core';
 import classes from './EventBanner.module.css';
 import useCheckEvents from '../../hooks/CheckEvents.ts/useCheckEvents';
-import { Image } from '@mantine/core';
-import events_blank from '../../images/events_blank.jpg';
+import blurred_event from '../../images/blurred_event.jpg';
 import { localDateTimeToDate } from '../../utils/localDateTimeToDate';
 
 
@@ -10,28 +9,41 @@ export function EventBanner() {
     const [events] = useCheckEvents();
     if (Array.isArray(events) && events.length > 0) {
         return (
-            <Card radius="lg" className={classes.card}>
-                <Image src={events[0].logo.url} fit="contain" radius="lg" h="auto" fallbackSrc={events_blank}/>
-                {/* <Overlay className={classes.overlay} opacity={0.55} zIndex={0} /> */}
-
-                <div className={classes.content}>
-                    <Text size="lg" fw={700} className={classes.title}>
-                        {events[0].name.text}
-                    </Text>
-                    <Grid>
-                    <Text size="lg" className={classes.description}>
-                        {events[0].description.text}
-                    </Text>
-                    <Text>
-                        {localDateTimeToDate(events[0].start.local)}
-                    </Text>
-                    </Grid>
-
-                    <Button className={classes.action} variant="filled" color="white" size="lg" onClick={() => window.location.href = `${events[0].url}`}>
-                        Register Today!
-                    </Button>
-                </div>
-            </Card>
+            <div className={classes.wrapper}>
+                <Box maw={1000} mx="auto" mah={300} my="auto" >
+                    <BackgroundImage
+                        src={events[0].logo.url ? events[0].logo.url : blurred_event}
+                        radius="md"
+                        component="button"
+                        onClick={() => window.location.href = `${events[0].url}`}>
+                        <Container className={classes.container}>
+                            <Group justify="space-between">
+                                <Text className={classes.title}
+                                    variant="gradient"
+                                    gradient={{ from: 'indigo', to: 'green', deg: 75 }}>
+                                    {events[0].name.text}
+                                </Text>
+                                <Badge size="xl" color="teal"
+                                    styles={{
+                                        label: { color: 'white' }
+                                    }}>
+                                    {localDateTimeToDate(events[0].start.local)}
+                                </Badge>
+                            </Group>
+                            <Text size="lg" c="white" fw={600}>
+                                {events[0].description.text}
+                            </Text>
+                            <Space h="md" />
+                            <Button
+                                variant="default"
+                                radius="md"
+                                onClick={() => window.location.href = `${events[0].url}`}>
+                                Click to Learn More
+                            </Button>
+                        </Container>
+                    </BackgroundImage>
+                </Box>
+            </div>
         );
     }
     return (
