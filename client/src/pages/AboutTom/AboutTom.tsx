2524f12718f8032bc10b37d69573f6820e351861
Steven Tran <dpoopd@gmail.com>
2023-11-26 09:52:39 -0800

    Removed comment & unused import

--- client/src/pages/AboutTom/AboutTom.tsx
+++ client/src/pages/AboutTom/AboutTom.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { HeroImageBackground } from '../../components/HeroImageBackground/HeroImageBackground';
-import { Title } from '@mantine/core';
 
 export const AboutTom = (): React.ReactElement => {
     return (
-        <div>
-            {/* <Title >About Tom</Title> */}
+        <>
             <HeroImageBackground />
-        </div>
+        </>
     );
 }
