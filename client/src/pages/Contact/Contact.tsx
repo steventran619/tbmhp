405235c806fac1d2ce59408b65801aaa4c6c7341
Steven Tran <dpoopd@gmail.com>
2023-11-26 15:09:17 -0800

    Added left/right padding to Contact page

--- client/src/pages/Contact/Contact.tsx
+++ client/src/pages/Contact/Contact.tsx
@@ -1,5 +1,6 @@
 import { TextInput, Textarea, SimpleGrid, Group, Title, Button, Text, Space } from '@mantine/core';
 import { useForm } from '@mantine/form';
+import classes from './Contact.module.css';
 
 export function Contact() {
     const form = useForm({
@@ -17,62 +18,64 @@ export function Contact() {
     });
 
     return (
-        <form onSubmit={form.onSubmit(() => { })}>
-            <Title
-                order={2}
-                size="h1"
-                style={{ fontFamily: 'Greycliff CF, var(--mantine-font-family)' }}
-                fw={900}
-                ta="center"
-            >
-                Contact Us
-            </Title>
-            <Space h="md" />
-            <Text >
-            We would love to hear from you! Just fill in the form below, and we will get back to you.
-            </Text>
-            <SimpleGrid cols={{ base: 1, sm: 2 }} mt="xl">
-                <TextInput
-                    label="Name"
-                    placeholder="Your name"
-                    name="name"
-                    variant="filled"
-                    {...form.getInputProps('name')}
-                />
-                <TextInput
-                    label="Email"
-                    placeholder="Your email"
-                    name="email"
-                    variant="filled"
-                    {...form.getInputProps('email')}
-                />
-            </SimpleGrid>
+        <div className={classes.wrapper}>
+            <form onSubmit={form.onSubmit(() => { })}>
+                <Title
+                    order={2}
+                    size="h1"
+                    style={{ fontFamily: 'Greycliff CF, var(--mantine-font-family)' }}
+                    fw={900}
+                    ta="center"
+                >
+                    Contact Us
+                </Title>
+                <Space h="md" />
+                <Text >
+                    We would love to hear from you! Just fill in the form below, and we will get back to you.
+                </Text>
+                <SimpleGrid cols={{ base: 1, sm: 2 }} mt="xl">
+                    <TextInput
+                        label="Name"
+                        placeholder="Your name"
+                        name="name"
+                        variant="filled"
+                        {...form.getInputProps('name')}
+                    />
+                    <TextInput
+                        label="Email"
+                        placeholder="Your email"
+                        name="email"
+                        variant="filled"
+                        {...form.getInputProps('email')}
+                    />
+                </SimpleGrid>
 
-            <TextInput
-                label="Subject"
-                placeholder="Subject"
-                mt="md"
-                name="subject"
-                variant="filled"
-                {...form.getInputProps('subject')}
-            />
-            <Textarea
-                mt="md"
-                label="Message"
-                placeholder="Your message"
-                maxRows={10}
-                minRows={5}
-                autosize
-                name="message"
-                variant="filled"
-                {...form.getInputProps('message')}
-            />
+                <TextInput
+                    label="Subject"
+                    placeholder="Subject"
+                    mt="md"
+                    name="subject"
+                    variant="filled"
+                    {...form.getInputProps('subject')}
+                />
+                <Textarea
+                    mt="md"
+                    label="Message"
+                    placeholder="Your message"
+                    maxRows={10}
+                    minRows={5}
+                    autosize
+                    name="message"
+                    variant="filled"
+                    {...form.getInputProps('message')}
+                />
 
-            <Group justify="center" mt="xl">
-                <Button type="submit" size="md" variant="default" color="green">
-                    Send message
-                </Button>
-            </Group>
-        </form>
+                <Group justify="center" mt="xl">
+                    <Button type="submit" size="md" variant="default" color="green">
+                        Send message
+                    </Button>
+                </Group>
+            </form>
+        </div>
     );
 }
